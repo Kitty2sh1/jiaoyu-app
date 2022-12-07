cafbf701669a9f52ce11283e7f4c12a87a80b106
Kitty2sh1 <2747993265@qq.com>
2022-12-07 15:20:56 +0800

    首页优惠券组件渲染

--- api/home.js
+++ api/home.js
@@ -1,11 +1,19 @@
 import http from "@/utils/request.js"
 export default {
-	// 获取首页数据
+	// 获取首页数据api
 	getHomeList(data) {
 		return http.request({
 			url: '/index',
 			method: 'GET',
 			data
 		})
-	}
+	},
+	// 获取可用优惠券列表api
+	getCouponList(data) {
+		return http.request({
+			url: '/coupon',
+			method: 'GET',
+			data
+		})
+	},
 }
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -4,6 +4,8 @@
 		<i-banner :bannerList="bannerList"></i-banner>
 		<!-- 图标分类组件 -->
 		<i-nav-bar :navBarList="navBarList"></i-nav-bar>
+		<!-- 可用优惠券组件 -->
+		<i-coupon :couponList="couponList"></i-coupon>
 	</view>
 </template>
 
@@ -12,6 +14,8 @@
 	import iBanner from '../../components/common/i-banner.vue';
 	// 图标分类组件
 	import iNavBar from '../../components/common/i-navBar.vue'
+	// 可用优惠券组件
+	import iCoupon from '../../components/common/i-coupon.vue'
 	// 首页api
 	import homeApi from '@/api/home.js'
 	export default {
@@ -20,17 +24,21 @@
 				bannerList: [], //轮播图
 				homeList: [], //首页数据
 				navBarList: [], //图标分类
+				couponList: [], //优惠券
 			}
 		},
 		onLoad() {
+			// 首页数据api
 			this.handleHomeList()
+			// 可用优惠券api
+			this.handleCouponList()
 		},
 		methods: {
 			// 首页数据api
 			async handleHomeList() {
 				try {
 					const res = await homeApi.getHomeList()
-					console.log('首页数据---', res.data);
+					// console.log('首页数据---', res.data);
 					if (res.data.code != 20000) {
 						uni.showToast({
 							title: res.data.data,
@@ -39,7 +47,7 @@
 						})
 					} else {
 						this.homeList = res.data.data
-						console.log(this.homeList);
+						// console.log(this.homeList);
 						this.homeList.forEach(item => {
 							if (item.type == 'swiper') {
 								this.bannerList = item.data
@@ -48,6 +56,24 @@
 								this.navBarList = item.data
 							}
 						})
+					}
+				} catch (e) {
+					//TODO handle the exception
+				}
+			},
+			// 可用优惠券api
+			async handleCouponList() {
+				try {
+					const res = await homeApi.getCouponList()
+					console.log('优惠券---', res.data);
+					if (res.data.code != 20000) {
+						uni.showToast({
+							title: res.data.data,
+							icon: 'none',
+							duration: 1500
+						})
+					} else {
+						this.couponList = res.data.data
 					}
 				} catch (e) {
 					//TODO handle the exception
@@ -62,7 +88,8 @@
 		},
 		components: {
 			iBanner,
-			iNavBar
+			iNavBar,
+			iCoupon
 		}
 	}
 </script>
